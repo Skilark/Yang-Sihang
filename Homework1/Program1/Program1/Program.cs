1894cb72cc847a3a650dd87a96e2a375a72d73dc
Skilark <2017302580142@whu.edu.cn>
2018-09-11 00:11:23 +0800

    This is my Homework1
    There are two programs in this file.

---
+++ Homework1/Program1/Program1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "";
+            string B = "";
+            Console.Write("Please input a number: ");
+            A = Console.ReadLine();
+            double a = Double.Parse(A);
+            Console.Write("Please input another number: ");
+            B = Console.ReadLine();
+            double b = Double.Parse(B);
+            Console.WriteLine("The product of the numbers is: " + (a * b));
+        }
+    }
+}
---
+++ Homework1/Program2/Program2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string number1 = textBox1.Text;
+            string number2 = textBox2.Text;
+            double number3 = Double.Parse(number1);
+            double number4 = Double.Parse(number2);
+            double product = number3 * number4;
+            textBox3.Text = product.ToString();
+        }
+    }
+}
