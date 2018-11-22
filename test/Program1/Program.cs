100cabda18af279c53636d6dd4d40e6ceed1a5ed
Skilark <2017302580142@whu.edu.cn>
2018-11-22 21:29:12 +0800

    This is my Homework9.
    There is a program in this file.

--- test/Program1/Program.cs
+++ test/Program1/Program.cs
@@ -1,104 +1,235 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Net;
-using System.Collections;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Diagnostics;
-using System.Collections.Concurrent;
 
-//将书中例9-10的爬虫程序，使用并行处理进行优化，实现更快速的网页爬取
-namespace SimpleCrawler
+using System.Text;
+
+using System.IO;
+
+using System.Net;
+
+using System.Collections;
+
+using System.Text.RegularExpressions;
+
+using System.Threading;
+
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+
+
+namespace program1
+
 {
-    public class Crawler
+
+    public class Program
+
     {
+
         private Hashtable urls;
+
         private int count;
 
+
+
         static void Main(string[] args)
+
         {
-            Crawler myCrawler = new Crawler();
 
-            string startUrl = "http://www.cnblogs.com/dstang2000/";
-            if (args.Length >= 1) startUrl = args[0];
+            Program myCrawler = new Program();
 
-            myCrawler.urls.Add(startUrl, false);        //加入初始页面
-            // 原代码  new Thread(myCrawler.Crawl).Start();        //开始爬行
-            myCrawler.Crawl();      //开始爬行
+
+
+            string startUrl = "https://www.csdn.net/nav/ai";
+
+
+
+            if (args.Length >= 1)
+
+            {
+
+                startUrl = args[0];
+
+            }
+
+
+
+            myCrawler.urls.Add(startUrl, false);
+
+
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            myCrawler.Crawl();
+
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
         }
 
-        public Crawler()
+
+
+        public Program()
+
         {
+
             urls = new Hashtable();
+
             count = 0;
+
         }
+
+
 
         private void Crawl()
-        {
-            Console.WriteLine("开始爬行了....");
 
-            //使用并行处理进行优化
+        {
+
+            Console.WriteLine("爬行开始了....");
+
+
 
             Parallel.For(0, 10, i =>
+
             {
+
                 string current = null;
+
+
+
                 lock (this)
+
                 {
-                    foreach (string url in urls.Keys)        //找到一个还没有下载过的链接
+
+                    foreach (string url in urls.Keys)
+
                     {
-                        if ((bool)urls[url]) continue;      //已经下载过的，不再下载
+
+                        if ((bool)urls[url])
+
+                        {
+
+                            continue;
+
+                        }
+
                         current = url;
+
                     }
+
+
 
                     Console.WriteLine("爬行" + current + "页面！");
 
-                    string html = DownLoad(current);        //下载
+
+
+                    string html = DownLoad(current);
+
+
+
                     urls[current] = true;
+
+
+
                     count = i;
 
-                    Parse(html);                            //解析，并加入新的链接
+
+
+                    Parse(html);
+
                 }
+
             });
 
 
-            Console.WriteLine("爬行结束");
+
+            Console.WriteLine("爬行结束\n");
+
         }
+
+
 
         public string DownLoad(string url)
+
         {
+
             try
+
             {
+
                 WebClient webClient = new WebClient();
+
                 webClient.Encoding = Encoding.UTF8;
+
                 string html = webClient.DownloadString(url);
 
+
+
                 string fileName = count.ToString();
+
                 File.WriteAllText(fileName, html, Encoding.UTF8);
+
                 return html;
+
             }
-            catch(Exception ex)
+
+            catch (Exception exception)
+
             {
-                Console.WriteLine(ex.Message);
+
+                Console.WriteLine(exception.Message);
+
                 return "";
+
             }
+
         }
+
+
 
         public void Parse(string html)
+
         {
+
             string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+
             MatchCollection matches = new Regex(strRef).Matches(html);
-            //使用并行处理进行优化
+
             Parallel.For(0, matches.Count, i =>
-              {
-                  strRef = matches[i].Value.Substring(matches[i].Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
-                  if (strRef.Length == 0) return;
-                  if (urls[strRef] == null) urls[strRef] = false;
-              });
+
+            {
+
+                strRef = matches[i].Value.Substring(matches[i].Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+
+
+
+                if (strRef.Length == 0)
+
+                {
+
+                    return;
+
+                }
+
+
+
+                if (urls[strRef] == null)
+
+                {
+
+                    urls[strRef] = false;
+
+                }
+
+            });
 
         }
+
     }
+
 }
